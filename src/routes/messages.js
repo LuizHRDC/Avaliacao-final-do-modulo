17da30a2d7ce29c5dc6802bfd1feeb7ff88ae99e
Luiz Henrique <luizhrdc137@gmail.com>
2024-10-26 15:00:37 -0300

    feat: adicionada paginacao

--- src/routes/messages.js
+++ src/routes/messages.js
@@ -24,19 +24,56 @@ router.post('/message', messageRegistration, (req, res) => {
     });
 });
 
-router.get('/message/:email', readMessages, (req, res) => {
-    const { email } = req.params;
+router.get('/messages/:email', (request, response) => {
 
-    const existingUser = users.find(user => user.email === email);
+    const { email } = request.params
 
-    const userMessages = messages.filter(msg => msg.email === existingUser.email);
-    if (userMessages !== -1) {
-        res.status(200).json({
-            message: `Seja bem-vindo!`,
-            notes: userMessages
-        });
+    const { page, perPage } = request.query
+
+    const user = users.find(user => user.email === email)
+    
+    if(!user){
+        return response.status(404).json({
+            message: 'Usuário não encontrado.'
+        })
     }
-});
+
+    const currentPage = parseInt(page) || 1 // padrão 1
+    const itemsPerPage = parseInt(perPage) || 10 // padrão 10
+
+    const userNotes = messages.filter(message => message.email === email)
+
+    const totalItems = userNotes.length
+
+    const startIndex = (currentPage - 1) * itemsPerPage
+
+    const endIndex = startIndex + itemsPerPage
+
+    const paginatedNotes = userNotes.slice(startIndex, endIndex)
+
+    const totalPages = Math.ceil(totalItems/itemsPerPage)
+
+    response.status(200).json({
+        messages: paginatedNotes,
+        totalPages,
+        currentPage
+    })
+
+})
+
+// router.get('/message/:email', readMessages, (req, res) => {
+//     const { email } = req.params;
+
+//     const existingUser = users.find(user => user.email === email);
+
+//     const userMessages = messages.filter(msg => msg.email === existingUser.email);
+//     if (userMessages !== -1) {
+//         res.status(200).json({
+//             message: `Seja bem-vindo!`,
+//             notes: userMessages
+//         });
+//     }
+// });
 
 router.put('/message/:id', updateMessages, (req, res) => {
     const { id } = req.params;
